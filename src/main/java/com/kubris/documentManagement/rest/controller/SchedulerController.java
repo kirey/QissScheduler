91857140261224ec4cf003b6c7fb8ffba231b047
Natalija <kapljicaa@gmail.com>
2018-04-20 10:34:38 +0200

    B: Added new empty SchedulerController

---
+++ src/main/java/com/kubris/documentManagement/rest/controller/SchedulerController.java
@@ -0,0 +1,14 @@
+package com.kubris.documentManagement.rest.controller;
+
+//  /scheduler
+public class SchedulerController {
+
+	
+	// jobs
+	// startJob 
+	// stopJob
+	// addJob
+	// editJob
+	// statusJob
+	// deleteJob
+}
