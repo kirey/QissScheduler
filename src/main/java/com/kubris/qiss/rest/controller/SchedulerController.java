5398f28bfb9d13f8b51de1174aa84e8a13cafe6f
Nikola J <n.jovanovic@dyntechdoo.com>
2018-04-24 10:48:51 +0200

    B: Edited stop and start method in rest controller

--- src/main/java/com/kubris/qiss/rest/controller/SchedulerController.java
+++ src/main/java/com/kubris/qiss/rest/controller/SchedulerController.java
@@ -13,6 +13,8 @@ import org.quartz.JobDetail;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
+import org.quartz.TriggerKey;
+import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -39,26 +41,27 @@ public class SchedulerController {
 
 	@Autowired
 	private SchedulersDao schedulersDao;
-	
+
 	@Autowired
 	private SchedulerExecutionLogDao schedulerExecutionLogDao;
-	
+
 	@Autowired
 	private Scheduler scheduler1;
-	
+
 	@Autowired
 	private SchedJobListener schedJobListener;
-	
-	
-	/*@Autowired 
-	@Qualifier("jobDetail1")
-	private JobDetail jobDetail1;*/
+
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier("jobDetail1") private JobDetail jobDetail1;
+	 */
 
 	@RequestMapping(value = "/jobs", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Schedulers> getAllSchedulers() {
 		return schedulersDao.findAll();
 	}
-	
+
 	@RequestMapping(value = "/job/{jobId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Schedulers getJobById(@PathVariable int jobId) {
 		return schedulersDao.findById(jobId);
@@ -75,7 +78,7 @@ public class SchedulerController {
 		schedulersDao.attachDirty(scheduler);
 		return "ok";
 	}
-	
+
 	@RequestMapping(value = "/deleteJob/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String deleteScheduler(@PathVariable int id) {
 		schedulersDao.delete(schedulersDao.findById(id));
@@ -84,68 +87,70 @@ public class SchedulerController {
 
 	/**
 	 * get scheduler log details
+	 * 
 	 * @param idLog
 	 * @return
 	 */
 	@RequestMapping(value = "/jobLog/{idLog}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	 public SchedulerExecutionLog getJobLog(@PathVariable int idLog) {
+	public SchedulerExecutionLog getJobLog(@PathVariable int idLog) {
 		return schedulerExecutionLogDao.findById(idLog);
-	 }
-	
+	}
+
 	@RequestMapping(value = "/jobHistory/{idJob}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	 public List<SchedulerExecutionLog> getHistoryByJobId(@PathVariable int idJob) {
-	  List<SchedulerExecutionLog> listHistoryByJobId = schedulerExecutionLogDao.getLogsByJobId(idJob);
-	  
-	  return listHistoryByJobId;
-	 }
-	
+	public List<SchedulerExecutionLog> getHistoryByJobId(@PathVariable int idJob) {
+		List<SchedulerExecutionLog> listHistoryByJobId = schedulerExecutionLogDao.getLogsByJobId(idJob);
+
+		return listHistoryByJobId;
+	}
+
 	/**
 	 * Method for starting scheduler/job
+	 * 
 	 * @param id
 	 * @return
 	 * @throws SchedulerException
 	 */
 	@RequestMapping(value = "/startJob/{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> startJob(@PathVariable int id) throws SchedulerException {
-		
-		Schedulers schedulerEnt = schedulersDao.findById(id);
-		CronTrigger cronTrigger = newTrigger().withIdentity("TRIGGER", "group1").withSchedule(cronSchedule(schedulerEnt.getCronExpression())).build();
-		
-		JobDetail jobDetail1 = newJob().ofType(SampleJob1.class).storeDurably().withIdentity(JobKey.jobKey(schedulerEnt.getJobName())).withDescription("Invoke Sample Job service...").build();
 
-				
+		Schedulers schedulerEnt = schedulersDao.findById(id);
+		CronTrigger cronTrigger = newTrigger().withIdentity(schedulerEnt.getJobName(), "group1")
+				.withSchedule(cronSchedule(schedulerEnt.getCronExpression())).build();
+
+		JobDetail jobDetail1 = newJob().ofType(SampleJob1.class).storeDurably()
+				.withIdentity(JobKey.jobKey(schedulerEnt.getJobName())).withDescription("Invoke Sample Job service...")
+				.build();
+
 		scheduler1.getListenerManager().addJobListener(schedJobListener);
 
-		scheduler1.scheduleJob(jobDetail1, cronTrigger);
-		scheduler1.start();
-		
-		//update status u scheduler
+		if (!scheduler1.checkExists(jobDetail1.getKey())) {
+			scheduler1.scheduleJob(jobDetail1, cronTrigger);
+			scheduler1.start();
+		} else {
+			scheduler1.resumeJob(JobKey.jobKey(schedulerEnt.getJobName()));
+		}
+
+		// update status u scheduler
 		schedulerEnt.setStatus(AppConstants.SCHEDULER_STATUS_STARTED);
 		schedulersDao.attachDirty(schedulerEnt);
-		
-		return new ResponseEntity("Job started", HttpStatus.OK); 
+
+		return new ResponseEntity("Job started", HttpStatus.OK);
 	}
-	
+
 	@RequestMapping(value = "/stopJob/{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> stopJob(@PathVariable int id) throws SchedulerException {
-		
+
 		Schedulers schedulerEnt = schedulersDao.findById(id);
-		scheduler1.interrupt(JobKey.jobKey(schedulerEnt.getJobName()));
-		
+		scheduler1.pauseJob(JobKey.jobKey(schedulerEnt.getJobName()));
 		schedulerEnt.setStatus(AppConstants.SCHEDULER_STATUS_FINISHED);
 		schedulersDao.attachDirty(schedulerEnt);
-		SchedulerExecutionLog schedulerLogForUpdate = schedulerExecutionLogDao.getLatestLogByJob(schedulerEnt.getJobName());
+		SchedulerExecutionLog schedulerLogForUpdate = schedulerExecutionLogDao
+				.getLatestLogByJob(schedulerEnt.getJobName());
 		schedulerLogForUpdate.setStatus(AppConstants.JOB_STATUS_INTERRUPT);
 		schedulerLogForUpdate.setEndTimestamp(new Date());
-		scheduler1.shutdown();
-		
-		return new ResponseEntity("Job stopped", HttpStatus.OK); 
+		schedulerExecutionLogDao.attachDirty(schedulerLogForUpdate);
+
+		return new ResponseEntity("Job stopped", HttpStatus.OK);
 	}
-	
-	
-	
-	
-	
-	
 
 }
