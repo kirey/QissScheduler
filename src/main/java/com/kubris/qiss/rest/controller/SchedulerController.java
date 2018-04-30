f71c52fc92b23591e3d56bfcd96db1b9ead1efb2
Nikola J <n.jovanovic@dyntechdoo.com>
2018-04-30 10:03:46 +0200

    B: set to return updated list when update,delete and add

--- src/main/java/com/kubris/qiss/rest/controller/SchedulerController.java
+++ src/main/java/com/kubris/qiss/rest/controller/SchedulerController.java
@@ -2,12 +2,7 @@ package com.kubris.qiss.rest.controller;
 
 import java.util.List;
 
-import org.quartz.JobExecutionContext;
-import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import org.quartz.UnableToInterruptJobException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,7 +18,6 @@ import com.kubris.qiss.data.dao.SchedulersDao;
 import com.kubris.qiss.data.dto.ResponseDto;
 import com.kubris.qiss.data.entity.SchedulerExecutionLog;
 import com.kubris.qiss.data.entity.Schedulers;
-import com.kubris.qiss.features.quartz.jobs.SchedJobListener;
 import com.kubris.qiss.rest.service.JobService;
 import com.kubris.qiss.utils.AppConstants;
 
@@ -38,18 +32,11 @@ public class SchedulerController {
 	private SchedulerExecutionLogDao schedulerExecutionLogDao;
 
 	@Autowired
-	private Scheduler scheduler1;
-
-	@Autowired
-	private SchedJobListener schedJobListener;
-	
-	@Autowired
 	JobService jobService;
-	
-	Logger logger = LoggerFactory.getLogger(getClass());
 
 	/**
 	 * get all jobs
+	 * 
 	 * @return
 	 */
 	@RequestMapping(value = "/jobs", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -59,6 +46,7 @@ public class SchedulerController {
 
 	/**
 	 * get job details
+	 * 
 	 * @param jobId
 	 * @return
 	 */
@@ -69,37 +57,40 @@ public class SchedulerController {
 	}
 
 	/**
-	 * add new job
+	 * add new job and return updated list of schedulers
+	 * 
 	 * @param scheduler
 	 * @return
 	 */
 	@RequestMapping(value = "/addJob", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ResponseDto> addScheduler(@RequestBody Schedulers scheduler) {
+	public ResponseEntity<List<Schedulers>> addScheduler(@RequestBody Schedulers scheduler) {
 		scheduler.setStatus(AppConstants.SCHEDULER_STATUS_INACTIVE);
 		schedulersDao.persist(scheduler);
-		return new ResponseEntity<ResponseDto>( new ResponseDto("successfully added"), HttpStatus.OK);
+		return new ResponseEntity<List<Schedulers>>(schedulersDao.findAll(), HttpStatus.OK);
 	}
 
 	/**
-	 * Edit job by id
+	 * Edit job by id and return updated list of schedulers
+	 * 
 	 * @param scheduler
 	 * @return
 	 */
 	@RequestMapping(value = "/editJob", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ResponseDto> editScheduler(@RequestBody Schedulers scheduler) {
+	public ResponseEntity<List<Schedulers>> editScheduler(@RequestBody Schedulers scheduler) {
 		schedulersDao.attachDirty(scheduler);
-		return new ResponseEntity<ResponseDto>( new ResponseDto("successfully edited"), HttpStatus.OK);
+		return new ResponseEntity<List<Schedulers>>(schedulersDao.findAll(), HttpStatus.OK);
 	}
 
 	/**
-	 * delete specific job
+	 * delete specific job and return updated list of schedulers
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@RequestMapping(value = "/deleteJob/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ResponseDto> deleteScheduler(@PathVariable int id) {
+	public ResponseEntity<List<Schedulers>> deleteScheduler(@PathVariable int id) {
 		schedulersDao.delete(schedulersDao.findById(id));
-		return new ResponseEntity<ResponseDto>(new ResponseDto("successfully deleted"), HttpStatus.OK);
+		return new ResponseEntity<List<Schedulers>>(schedulersDao.findAll(), HttpStatus.OK);
 	}
 
 	/**
@@ -112,64 +103,57 @@ public class SchedulerController {
 	public ResponseEntity<SchedulerExecutionLog> getJobLog(@PathVariable int idLog) {
 		SchedulerExecutionLog sel = schedulerExecutionLogDao.findById(idLog);
 		return new ResponseEntity<SchedulerExecutionLog>(sel, HttpStatus.OK);
-	 }
-	
+	}
+
 	/**
 	 * returns all logs for selected job
+	 * 
 	 * @param idJob
 	 * @return
 	 */
 	@RequestMapping(value = "/jobHistory/{idJob}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<SchedulerExecutionLog>> getHistoryByJobId(@PathVariable int idJob) {
-	  List<SchedulerExecutionLog> listHistoryByJobId = schedulerExecutionLogDao.getLogsByJobId(idJob);
-	  return new ResponseEntity<List<SchedulerExecutionLog>>(listHistoryByJobId, HttpStatus.OK);
-	 }
+		List<SchedulerExecutionLog> listHistoryByJobId = schedulerExecutionLogDao.getLogsByJobId(idJob);
+		return new ResponseEntity<List<SchedulerExecutionLog>>(listHistoryByJobId, HttpStatus.OK);
+	}
 
 	/**
 	 * Method for starting scheduler/job
 	 * 
 	 * @param id
 	 * @return
+	 * @throws ClassNotFoundException 
 	 * @throws SchedulerException
 	 */
 	@RequestMapping(value = "/startJob/{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ResponseDto> startJob(@PathVariable int id) {
-		
+	public ResponseEntity<ResponseDto> startJob(@PathVariable int id) throws ClassNotFoundException {
+
 		try {
 			jobService.startJob(id);
-			return new ResponseEntity<ResponseDto>( new ResponseDto("Job started"), HttpStatus.OK); 
-		} catch (SchedulerException | ClassNotFoundException e) {
-			return new ResponseEntity<ResponseDto>( new ResponseDto("Job starting failed " + e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR); 
+			return new ResponseEntity<ResponseDto>(new ResponseDto("Job started"), HttpStatus.OK);
+		} catch (SchedulerException e) {
+			return new ResponseEntity<ResponseDto>(new ResponseDto("Job starting failed"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	/**
-	 * Method for stopping job with given id
+	 * MEthod for stopping job with given id
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@RequestMapping(value = "/stopJob/{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ResponseDto> stopJob(@PathVariable Integer id) {
-		
-		try {			
-			jobService.stopJob(id);
-			return new ResponseEntity<ResponseDto>( new ResponseDto("Job stopped sucesffully"), HttpStatus.OK); 
-		} catch ( SchedulerException e) {
-			return new ResponseEntity<ResponseDto>( new ResponseDto("Failed to stop job: " + e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR); 
-		}
-		
-	}
-	
-	//not implemented yet
-	@RequestMapping(value = "/pauseJob/{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ResponseDto> pauseJob(@PathVariable int id) {
-		
+
 		try {
-			jobService.pauseJob(id);
-			return new ResponseEntity<ResponseDto>( new ResponseDto("Job paused"), HttpStatus.OK); 
+			jobService.stopJob(id);
+			return new ResponseEntity<ResponseDto>(new ResponseDto("Job stopped sucesffully"), HttpStatus.OK);
 		} catch (SchedulerException e) {
-			return new ResponseEntity<ResponseDto>( new ResponseDto("Failed to pause job"), HttpStatus.INTERNAL_SERVER_ERROR); 
+			return new ResponseEntity<ResponseDto>(new ResponseDto("Job starting failed: " + e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+
 	}
-	
+
 }
